211533234bc75b6da522965217b226bb89be8af7
kajun <kjstudios858@gmail.com>
2014-02-10 00:59:44 -0800

    add request and tip calculating functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,8 +11,17 @@ def home():
 
 @app.route('/results', methods=['POST'])
 def results():
-    return render_template('results.html')
+    meal_cost = request.form['meal_cost']
+    tip_percentage = request.form['tip_percentage']
+    total_cost = tip_calculator(meal_cost, tip_percentage)
+    return render_template('results.html', total_cost=total_cost)
 
+
+def tip_calculator(meal_cost, tip_percentage):
+    meal_cost = float(meal_cost)
+    tip_percentage = float(tip_percentage)
+    tip_cost = meal_cost * (tip_percentage/100)
+    return meal_cost + tip_cost
 
 if __name__ == '__main__':
     app.run(debug=True)
